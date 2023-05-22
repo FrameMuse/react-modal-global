2c8f4cf74c57768925332fbe2244f70935cc90a0
Valery Zinchenko <minicablestone@gmail.com>
2023-05-22 18:06:21 +0300

    Merge branch 'main' of https://github.com/FrameMuse/react-modal-global into experimental

--- src/hooks.ts
+++ src/hooks.ts
@@ -28,14 +28,16 @@ import { ModalComponent, ModalState } from "./types"
  *
  * Accepts a generic type that is used to infer the props of the modal component.
  * It has 3 overloads:
- * 1. `useModalWindow<ModalComponent>()` - infers the props from the class component type.
- * 2. `useModalWindow<typeof ModalComponent>()` - infers the props from the function component type.
- * 3. `useModalWindow<unknown>()` - infers any type besides the above.
+ * 1. `useModalWindow<typeof ModalClassComponent>()` - infers the props from the class component type.
+ * 2. `useModalWindow<typeof ModalFunctionComponent>()` - infers the props from the function component type.
+ * 3. `useModalWindow<{ c: 3 }>()` - you can enter props by yourself too.
  */
 export function useModalWindow<T>(): ModalWindow<T extends ModalComponent<infer Props> ? Props : T> {
   const context = useContext(modalContext)
-  if (!context) throw new Error("ModalError: useModalWindow must be used within a modal context")
+  if (!context) throw new Error(`ModalError: ${useModalWindow.name} must be used within a modal context.`)
 
+  // It's safe to case to `any` here because the context is always set to a `ModalWindow`
+  // and the arbitrary type `T` is difened by the user.
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   return context as ModalWindow<any>
 }
